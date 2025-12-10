bfb17448b6ca1f949652ef858b3e41750163da34
BrunoHenri1 <olivierabruno16@gmail.com>
2025-12-10 15:00:16 -0300

    update

--- Exer003/index.php
+++ Exer003/index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
-</head>
-<body>
     <?php 
         $txt = $_GET["txt"];
         $tam = $_GET["tam"];
         $cor = $_GET["cor"];
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
 
-        echo "$txt"
+    <style>
+        span.texto {
+            font-size: <?php echo $tam; ?>;
+            color: <?php  echo $cor; ?>;
+        }
+
+    </style>
+</head>
+<body>
+    <?php 
+        echo "<span class='texto'>$txt</span><br>" 
     ?>
     <button onclick="history.back()">Voltar</button>
     
