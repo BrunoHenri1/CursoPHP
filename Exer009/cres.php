817c7de2817c9a4356234b02fb77cfe8ad630dc4
BrunoHenri1 <olivierabruno16@gmail.com>
2025-12-18 16:15:44 -0300

    estudo php

---
+++ Exer009/cres.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador</title>
+</head>
+<body>
+    <?php 
+        $cont = 1;
+
+        while ($cont <= 10){
+            echo $cont .'<br>';
+            $cont++;
+        }
+    ?>
+</body>
+</html>
